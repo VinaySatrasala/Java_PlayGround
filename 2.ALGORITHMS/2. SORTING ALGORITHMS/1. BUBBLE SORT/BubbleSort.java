1a386b393c45beebc35b4039441a2a4772710c5a
vinay satrasala <outfit.vinay960@gmail.com>
2023-04-04 23:24:33 +0530

    sevev

--- 2.ALGORITHMS/2. SORTING ALGORITHMS/1. BUBBLE SORT/BubbleSort.java
+++ 2.ALGORITHMS/2. SORTING ALGORITHMS/1. BUBBLE SORT/BubbleSort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class BubbleSort{
     public static void main(String[] args) {
         int[] array={4,3,1,5,4,4,2};
-
+        Integer.bitCount(0);
         // For swapped array the loop will run for N^2 times to stop it after first parsing-->Give a boolean
         boolean swapped;
         for(int i=0;i<array.length-1;i++){
